f93fe55db39f2004aa1f1c7ed1f60df18491782b
Stephen Bizimana <stephen.bizimana@gmail.com>
2019-02-08 03:37:42 -0500

    adding left side moves

--- Moves.py
+++ Moves.py
@@ -30,3 +30,33 @@ class Moves:
 
         return second
 
+    @staticmethod
+    def left(first, second, ccw):
+        # These are all the moves when turning the left side. In the list, each index contains a list containing two
+        # tuples. The first tuple is the starting coordinate for a piece of the cube, the next tuple is the finish
+        # coordinate.
+        left_moves = [[(0, 3), (2, 3)], [(3, 0), (3, 3)], [(11, 3), (3, 2)], [(0, 4), (1, 3)], [(11, 4), (3, 1)],
+                     [(0, 5), (0, 3)], [(11, 5), (3, 0)], [(3, 8), (11, 3)], [(1, 3), (2, 4)], [(3, 1), (3, 4)],
+                     [(1, 5), (0, 4)], [(3, 7), (11, 4)], [(2, 3), (2, 5)], [(3, 2), (3, 5)], [(3, 3), (3, 6)],
+                     [(2, 4), (1, 5)], [(3, 4), (3, 7)], [(2, 5), (0, 3)], [(3, 5), (3, 8)], [(3, 6), (11, 5)]]
+
+        # if counterclockwise, flip all the starting and finishing coordinates, effectively turning the side in the
+        # other direction.
+        if ccw:
+            left_moves_r = left_moves
+            for x in range(len(left_moves_r)):
+                left_moves_r[x] = [left_moves_r[x][1], left_moves_r[x][0]]
+
+            for move in left_moves_r:
+                first[move[0][0]][move[0][1]] = second[move[1][0]][move[1][1]]
+        else:
+            for move in left_moves:
+                first[move[0][0]][move[0][1]] = second[move[1][0]][move[1][1]]
+
+        for x in range(12):
+            for y in range(9):
+                if first[x][y] is not " ":
+                    second[x][y] = first[x][y]
+
+        return second
+
